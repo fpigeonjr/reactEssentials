e2dc7ed1f930997f38c6c476dd6e60540f8ef71e
Frank Pigeon <frank.pigeonjr@gmail.com>
2017-01-13 23:37:14 -0600

    sets up environment

--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,14 @@ module.exports = {
                 test: /\.json$/,
                 exclude: /(node_modules)/,
                 loader: "json-loader"
+            },
+            {
+                test: /\.css$/,
+                loader: 'style-loader!css-loader!autoprefixer-loader'
+            },
+            {
+                test: /\.scss$/, 
+                loader: 'style-loader!css-loader!autoprefixer-loader!sass-loader'
             }
         ]
     }
