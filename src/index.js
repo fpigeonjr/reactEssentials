7e21bf62a1af9425cb4e1b808212e37d6386186c
Frank Pigeon <frank.pigeonjr@gmail.com>
2017-01-15 23:22:04 -0600

    starts ski day row and list components

--- src/index.js
+++ src/index.js
@@ -1,14 +1,32 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { SkiDayCount } from './components/SkiDayCount'
+import { SkiDayList } from './components/SkiDayList'
 
 window.React = React
 
 render (
-    <SkiDayCount total={50}
-                powder={20}
-                backcountry={10}
-                goal={100}/>,
+    <SkiDayList days={
+        [
+            {
+                resort: "Squaw Valley",
+                date: new Date("1/2/2016"),
+                powder: true,
+                backcountry: false
+            },
+            {
+                resort: "Panama City",
+                date: new Date("2/15/2016"),
+                powder: false,
+                backcountry: true
+            },
+            {
+                resort: "Mexico City",
+                date: new Date("3/17/2016"),
+                powder: true,
+                backcountry: true
+            }
+        ]
+    }/>,
     document.getElementById('react-container')
 )
 
