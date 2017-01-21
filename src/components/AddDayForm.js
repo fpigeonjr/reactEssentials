fc89a60a01d0cca63054065030be79a298fb70d0
Frank Pigeon Jr <frank.pigeonjr@gmail.com>
2017-01-20 22:30:49 -0600

    corrects menu icons and flushes out form and submit logic

--- src/components/AddDayForm.js
+++ src/components/AddDayForm.js
@@ -1,28 +1,53 @@
 import { PropTypes, Component } from 'react'
 
 export class AddDayForm extends Component {
+
+    constructor(props) {
+        super(props)
+        this.submit = this.submit.bind(this)
+    }
+
+    submit(e) {
+        e.preventDefault();
+        console.log('resort', this.refs.resort.value);
+        console.log('date', this.refs.date.value);
+        console.log('powder', this.refs.powder.checked);
+        console.log('backcountry', this.refs.backcountry.checked);
+    }
+
     render() {
+
+        const { resort, date, powder, backcountry } = this.props
+
         return(
-            <form className="add-day-form">
-                <label htmlFor="resort">Resort Name</label> 
-                <input type="text" id="resort" required/>
-                
+            <form  onSubmit={this.submit} className="add-day-form">
+                <label htmlFor="resort">Resort Name</label>
+                <input type="text" id="resort" defaultValue={resort} ref="resort" required/>
+
                 <label htmlFor="date">Date</label>
-                <input type="date" id="date" required/>
-                
+                <input type="date" id="date" defaultValue={date} ref="date" required/>
+
                 <div>
-                    <input type="checkbox" id="powder"/>
+                    <input type="checkbox" id="powder" ref="powder" defaultChecked={powder}/>
                     <label htmlFor="powder">Powder Day</label>
                 </div>
 
                 <div>
-                    <input type="checkbox" id="backcountry"/>
+                    <input type="checkbox" id="backcountry" ref="backcountry" defaultChecked={backcountry}/>
                     <label htmlFor="backcountry">Backcountry Day</label>
-                </div>                
+                </div>
+                <button> Add Day </button>
             </form>
         )
     }
 
+}
+
+AddDayForm.defaultProps  = {
+    resort: "Panama",
+    date: "2017-02-12",
+    powder: true,
+    backcountry: false
 }
 
 AddDayForm.PropTypes = {
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -3,15 +3,15 @@ import HomeIcon from 'react-icons/lib/fa/home'
 import AddDayIcon from 'react-icons/lib/fa/calendar-plus-o'
 import ListDaysIcon from 'react-icons/lib/fa/table'
 
- export const Menu = () =>
-    <nav className="menu">
-        <Link to="/">
-            <HomeIcon activeClassName="selected"/>
-        </Link>
-        <Link to="/add-day">
-            <AddDayIcon activeClassName="selected"/>
-        </Link>
-        <Link to="/list-days">
-            <ListDaysIcon activeClassName="selected"/>
-        </Link>
-    </nav>
+export const Menu = () =>
+	<nav className="menu">
+		<Link to="/" activeClassName="selected">
+			<HomeIcon />
+		</Link>
+		<Link to="/add-day" activeClassName="selected">
+			<AddDayIcon />
+		</Link>
+		<Link to="/list-days" activeClassName="selected">
+			<ListDaysIcon />
+		</Link>
+	</nav>
